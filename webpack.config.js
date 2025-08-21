e4c117fcbf04d15a5dd1651de2cf8ff656b330b7
alexzhurov <alexzhurovbiz@gmail.com>
2025-08-21 23:56:38 +0200

    fix: fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,51 +2,50 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.tsx",
-  mode: "development",
-  output: {
-    filename: "bundle.[fullhash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-  resolve: {
-    modules: [__dirname, "src", "node_modules"],
-    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
-  },
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(js|ts)x?$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        exclude: /node_modules/,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-            },
-          },
-          "postcss-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-    ],
-  },
+	entry: "./src/index.tsx",
+	mode: "development",
+	output: {
+		filename: "bundle.[fullhash].js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+		}),
+	],
+	resolve: {
+		modules: [__dirname, "src", "node_modules"],
+		extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+	},
+	devtool: "inline-source-map",
+	module: {
+		rules: [
+			{
+				test: /\.(js|ts)x?$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				exclude: /node_modules/,
+				use: ["file-loader"],
+			},
+			{
+				test: /\.(sa|c|sc)ss$/,
+				use: [
+					// Creates `style` nodes from JS strings
+					"style-loader",
+					// Translates CSS into CommonJS
+					{
+						loader: "css-loader",
+						options: {
+							sourceMap: true,
+						},
+					},
+					// Compiles Sass to CSS
+					"sass-loader",
+				],
+			},
+		],
+	},
 };
